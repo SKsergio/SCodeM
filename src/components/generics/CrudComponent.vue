663ad2bec1e82e589934633a0808587501c0e61b
SKsergio <sergiofloress911@gmail.com>
2025-04-13 23:15:35 -0600

    change some classes wiirh affectation in generales

--- src/components/generics/CrudComponent.vue
+++ src/components/generics/CrudComponent.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="container">
-        <h1>Hola</h1>
+        <div class="header__general">
+            <h1>Degreess</h1>
+        </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 
 </script>
 
 <style scoped>
+    @import url('../../css/globalClases.css');
+    @import url('../../css/variables.css');
+    .header__general{
+        background-color: var(--color-sixth);
+        width: 100%;
+        height: 145px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        > h1{
+            color: rgb(236, 232, 225);
+            margin-left: 30px;
+        }
+    }
 </style>
